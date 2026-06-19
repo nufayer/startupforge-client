ffb21d5a3b0f9c8d07eaff821ed2d9b04dc6b35a
nufayer <nufayer6543@gmail.com>
2026-06-19 21:38:09 +0600

    Project initiated

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="dark"
+      className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
